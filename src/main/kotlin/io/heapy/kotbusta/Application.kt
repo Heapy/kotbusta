50adefb86346eb95c2d069b08d8851ce53140335
Ruslan Ibragimov <ruslan@heapy.io>
2025-08-11 12:07:29 +0300

    Switch to embeddedServer in `Application.kt`
    - Replaced `EngineMain.main` with `embeddedServer(CIO, ...)` for server startup.
    - Added `@JvmName("Application")` for consistent class naming.

--- src/main/kotlin/io/heapy/kotbusta/Application.kt
+++ src/main/kotlin/io/heapy/kotbusta/Application.kt
@@ -1,3 +1,5 @@
+@file:JvmName("Application")
+
 package io.heapy.kotbusta
 
 import io.heapy.kotbusta.ktor.configureAuthentication
@@ -7,9 +9,11 @@ import io.heapy.kotbusta.ktor.configureStatusPages
 import io.heapy.kotbusta.coroutines.DispatchersModule
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
+import io.ktor.server.engine.embeddedServer
 
 fun main() {
-    EngineMain.main(arrayOf())
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
 }
 
 fun Application.module() {
