b01af38078f74aa842279b1993f9a104e64c1e38
Ruslan Ibragimov <ruslan@heapy.io>
2025-09-19 22:50:35 +0300

    Use `ImportJobService` and transactional context in `GetJobsRoute`.

--- src/main/kotlin/io/heapy/kotbusta/ktor/routes/admin/GetJobsRoute.kt
+++ src/main/kotlin/io/heapy/kotbusta/ktor/routes/admin/GetJobsRoute.kt
@@ -1,6 +1,7 @@
 package io.heapy.kotbusta.ktor.routes.admin
 
 import io.heapy.kotbusta.ApplicationFactory
+import io.heapy.kotbusta.database.TransactionType.READ_ONLY
 import io.heapy.kotbusta.model.ApiResponse.Success
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,10 +9,15 @@ import io.ktor.server.routing.*
 context(applicationFactory: ApplicationFactory)
 fun Route.getJobsRoute() {
     val adminService = applicationFactory.adminService.value
+    val importJobService = applicationFactory.importJobService.value
+    val transactionProvider = applicationFactory.transactionProvider.value
 
     get("/jobs") {
         adminService.requireAdminRights {
-            val jobs = adminService.getAllJobs()
+            val jobs = transactionProvider.transaction(READ_ONLY) {
+                importJobService.getAllJobs()
+            }
+
             call.respond(
                 Success(
                     data = jobs,
