7e3edb9cbad13feaeabf126bba68768fd8e0f978
Ruslan Ibragimov <ruslan@heapy.io>
2025-10-30 12:59:49 +0300

    Simplify `TypeMapperTest` by removing unused imports and replacing enum references with explicit output values.

--- src/test/kotlin/io/heapy/kotbusta/mapper/TypeMapperTest.kt
+++ src/test/kotlin/io/heapy/kotbusta/mapper/TypeMapperTest.kt
@@ -2,8 +2,6 @@ package io.heapy.kotbusta.mapper
 
 import io.heapy.kotbusta.dao.JobStatusMapper
 import io.heapy.kotbusta.dao.JobTypeMapper
-import io.heapy.kotbusta.jooq.enums.JobStatusEnum
-import io.heapy.kotbusta.jooq.enums.JobTypeEnum
 import io.heapy.kotbusta.model.JobStatus
 import io.heapy.kotbusta.model.JobType
 import org.junit.jupiter.api.Test
@@ -13,7 +11,7 @@ class TypeMapperTest {
     fun `JobTypeMapper should be bidirectional`() {
         JobTypeMapper.verifyBidirectional(
             inputs = JobType.entries,
-            outputs = JobTypeEnum.entries.toList(),
+            outputs = listOf("DATA_IMPORT", "COVER_EXTRACTION"),
         )
     }
 
@@ -21,7 +19,7 @@ class TypeMapperTest {
     fun `JobStatusMapper should be bidirectional`() {
         JobStatusMapper.verifyBidirectional(
             inputs = JobStatus.entries,
-            outputs = JobStatusEnum.entries.toList(),
+            outputs = listOf("RUNNING", "COMPLETED", "FAILED"),
         )
     }
 }
